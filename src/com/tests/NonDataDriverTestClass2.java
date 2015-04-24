140ed6332b0bbaa5edcecc8623b7a94ca8e2db35
shirazny123 <shaider@LNYDAHHZTNN.cpg.org>
2015-04-24 09:44:21 -0400

    more changes

--- src/com/tests/NonDataDriverTestClass2.java
+++ src/com/tests/NonDataDriverTestClass2.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 import com.pageobjects.BaseClass;
 
 @Test
-public class NonDataDriverTestClass1 extends BaseClass {
+public class NonDataDriverTestClass2 extends BaseClass {
 
-	public void SeleniumNonDataDrivenTest1() {
+	public void SeleniumNonDataDrivenTest4() {
 		Assert.assertEquals("AAA", "AA");
 	}
 
-	public void SeleniumSimpleTest2() {
+	public void SeleniumSimpleTest5() {
 		Assert.assertEquals("AAA", "AAA");
 	}
 
-	public void SeleniumSimpleTest3() {
+	public void SeleniumSimpleTest6() {
 		Assert.assertEquals("AAA", "AAA");
 	}
 
